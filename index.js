daa74b0a12da0c2e90c2dc6742167fd091fed806
md-zeon <zeonrahaman5870@gmail.com>
2025-06-16 17:19:02 +0600

    added Functionality to get a single artifact

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@devcluster.s7bmtla.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -40,6 +40,14 @@ async function run() {
 			}
 			const cursor = artifactsCollection.find(query);
 			const result = await cursor.toArray();
+			res.send(result);
+		});
+
+		// get a single artifact
+		app.get("/artifacts/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await artifactsCollection.findOne(query);
 			res.send(result);
 		});
 
