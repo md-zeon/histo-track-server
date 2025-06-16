ab3a62bcdb2109b196c3d2d70d20ea5a565dcf9e
md-zeon <zeonrahaman5870@gmail.com>
2025-06-16 07:50:14 +0600

    MongoDB Set up Complete

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
-const express = require('express');
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('My Histrotrack Server is running!');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@devcluster.s7bmtla.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+async function run() {
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
+
+
+
+        
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log("Pinged your deployment. You successfully connected to MongoDB!");
+	} finally {
+		// Ensures that the client will close when you finish/error
+		// await client.close();
+	}
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+	res.send("My Histrotrack Server is running!");
 });
 
 app.listen(port, () => {
-    console.log(`My Histrotrack Server is running on port ${port}`);
-    console.log(`http://localhost:${port}`);
-})
+	console.log(`My Histrotrack Server is running on port ${port}`);
+	console.log(`http://localhost:${port}`);
+});
