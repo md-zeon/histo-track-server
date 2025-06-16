af3deb6dd25be9715cafb59be577c0ffc19571e6
md-zeon <zeonrahaman5870@gmail.com>
2025-06-17 00:43:40 +0600

    added Delete artifact Api

--- index.js
+++ index.js
@@ -112,6 +112,14 @@ async function run() {
 			res.send(likedArtifacts);
 		});
 
+        // delete an artifact
+        app.delete("/artifacts/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await artifactsCollection.deleteOne(query);
+            res.send(result);
+        })
+
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
 		console.log("Pinged your deployment. You successfully connected to MongoDB!");
